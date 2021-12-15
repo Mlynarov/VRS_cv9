3c6b886e438cba37ac433bd0f6001fdf040f9159
Mlynarov <michal.molnar98@gmail.com>
2021-12-15 21:17:24 +0100

    Doplnenie funkcii na citanie a zapisovanie cez i2c

---
+++ Core/Inc/lps25hb.h
@@ -0,0 +1,13 @@
+/*
+ * lps25hb.h
+ *
+ *  Created on: Dec 15, 2021
+ *      Author: Molnar a Nyiri
+ */
+
+#ifndef INC_LPS25HB_H_
+#define INC_LPS25HB_H_
+
+
+
+#endif /* INC_LPS25HB_H_ */
---
+++ Core/Src/lps25hb.c
@@ -0,0 +1,8 @@
+/*
+ * lps25hb.c
+ *
+ *  Created on: Dec 15, 2021
+ *      Author: Molnar a Nyiri
+ */
+
+#include "lps25hb.h"
